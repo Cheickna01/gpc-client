cb2dbe38d602d48fbd454ff233d988868f72def9
Cheickna01 <diabyhamala0@gmail.com>
2026-02-12 15:13:45 +0100

    fix bugs

--- src/api/config.jsx
+++ src/api/config.jsx
@@ -1,8 +1,8 @@
 // pour la production
-// export const base_url = "https://gpc-server-8pvt.onrender.com/api";
-// export const front_url = "https://gpc-ch.netlify.app";
+export const base_url = "https://gpc-server-8pvt.onrender.com/api";
+export const front_url = "https://gpc-ch.netlify.app";
 
 
 // pour le dev
-export const base_url = "http://localhost:4000/api";
-export const front_url = "http://localhost:5173";
+// export const base_url = "http://localhost:4000/api";
+// export const front_url = "http://localhost:5173";
